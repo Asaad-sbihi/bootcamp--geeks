d88f1cd9af6283628c35c29c6d3f801c0d120b41
Asaad-sbihi <asaadsbihhi737@gmail.com>
2025-05-07 13:19:36 +0200

    daiily.challenge

---
+++ week0/day1/dailly-chaellenger/daily.py
@@ -0,0 +1,18 @@
+# challengge 1
+number = int(input("Give me number"))
+length = int(input("Give me lenght"))
+
+numbers = []
+for i in range( 1,length+1) :
+   numbers.append(number * i)
+
+print(numbers)
+# challenge 2
+word = input('entrer votre mot :')
+result =""
+for lettre in word :
+   if not result or lettre != result[-1]:
+       result += lettre
+
+
+print(result)
