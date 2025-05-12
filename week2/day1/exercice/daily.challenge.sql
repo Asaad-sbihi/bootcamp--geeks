f0c63328dac7a045661f4013683d547c88a5cf57
Asaad-sbihi <asaadsbihhi737@gmail.com>
2025-05-12 18:15:29 +0200

    daily.challenge

---
+++ week2/day1/exercice/daily.challenge.sql
@@ -0,0 +1,40 @@
+-- CREATE TABLE actors(
+--  actor_id SERIAL PRIMARY KEY,
+--  first_name VARCHAR (50) NOT NULL,
+--  last_name VARCHAR (100) NOT NULL,
+--  age DATE NOT NULL,
+--  number_oscars SMALLINT NOT NULL
+-- )
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Matt','Damon','08/10/1970', 5);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('George','Clooney','06/05/1961', 2);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Khadija','sbihi','08/10/1970', 5);
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES('Lana','derey','06/05/1961', 2);
+
+
+
+
+
+-- INSERT INTO actors (first_name, last_name, age, number_oscars)
+-- VALUES
+-- ('Shakira','pique','01/15/1997', 5),
+-- ('Salma','Rachid','3/12/1999', 6),
+-- ('Fatma','Msoudi','5/13/1998', 7);
+
+
+-- SELECT * FROM actors;
+
+ -- Count how many actors are in the table.
+ -- SELECT count(*) FROM actors;
+
+ -- Try to add a new actor with some blank fields. What do you think the outcome will be ?
+ -- INSERT INTO actors (first_name, last_name, age, number_oscars)
+ -- VALUES(,,'06/05/1961', );    syntax error
+
+
