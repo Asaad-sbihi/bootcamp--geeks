b569a207dd07d645bb3bb0138f1797f5effd3eab
Asaad-sbihi <asaadsbihhi737@gmail.com>
2025-05-12 23:01:52 +0200

    day5

---
+++ week2/day1/exercice/exercice XP+.sql
@@ -0,0 +1,29 @@
+
+
+-- CREATE TABLE students (
+-- student_id SERIAL PRIMARY KEY,
+--   first_name VARCHAR(50),
+--   last_name VARCHAR(50),
+--   birth_date DATE
+-- );
+
+
+-- INSERT INTO students (first_name, last_name, birth_date) VALUES
+-- ('Marc', 'Benichou', '1998-11-02'),
+-- ('Yoan', 'Cohen', '2010-12-03'),
+-- ('Lea', 'Benichou', '1987-07-27'),
+-- ('Amelia', 'Dux', '1996-04-07'),
+-- ('David', 'Grez', '2003-06-14'),
+-- ('Omer', 'Simpson', '1980-10-03');
+
+-- select * from students
+
+-- INSERT INTO students (first_name, last_name, birth_date)
+-- VALUES ('Asaad', 'Sbihi', '04-20-2004');
+
+-- SELECT * FROM students;
+-- SELECT first_name, last_name FROM students;
+
+
+
+
