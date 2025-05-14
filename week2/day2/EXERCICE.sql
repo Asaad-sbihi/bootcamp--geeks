63f2a8e9514a152be7e5e553003839aefdbd1959
Asaad-sbihi <asaadsbihhi737@gmail.com>
2025-05-14 15:02:21 +0200

    day2

---
+++ week2/day2/EXERCICE.sql
@@ -0,0 +1,37 @@
+
+-- Exercice1
+-- SELECT * FROM items ORDER BY price ASC;
+--  SELECT * FROM items WHERE price >= 80 ORDER BY price DESC;
+--  SELECT first_name, last_name FROM customers ORDER BY first_name ASC LIMIT 3;
+--   SELECT last_name FROM customers ORDER BY last_name DESC;
+
+
+
+ex2
+-- select * from customer
+-- select (first_name, last_name) as full_name from customer
+-- select DISTINCT create_date from customer;
+
+-- SELECT * FROM customer ORDER BY first_name DESC;
+
+-- select film_id ,title,description,release_year,rental_rate 
+-- from film 
+-- order by rental_rate ASC;
+
+-- SELECT address, phone
+-- FROM address
+-- WHERE district = 'Texas';
+
+-- SELECT *
+-- FROM film
+-- WHERE film_id IN (15, 150);
+
+-- SELECT film_id, title, description, length, rental_rate
+-- FROM film
+-- WHERE title = 'fight club';
+
+
+
+
+
+
